94f6f940f76c658754c789180ff9febd7116cf0b
tamal15 <tamal15-13243@diu.edu.bd>
2022-07-13 00:37:36 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Shared/Header/Header';
+import Home from './Home/Home';
+
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+     <Header />
      <Routes>
-     <Route path="/" element={<Header />} />
+     <Route path="/" element={<Home />} />
+     
      </Routes>
      </BrowserRouter>
     </div>
---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './Banner/Banner';
+import BookGallery from './BookGallery/BookGallery';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <BookGallery></BookGallery>
+            
+        </div>
+    );
+};
+
+export default Home;
